fbef8dc1a5fae2f073954613feaac7c885648b81
Luarle Sousa <luarlelima@gmail.com>
2022-02-21 15:12:33 -0400

    Update for Keysight and Rohde-Schwarz process verification

--- main.py
+++ main.py
@@ -7,57 +7,65 @@ import winapps
 from ctypes import Structure, windll, c_uint, sizeof, byref
 
 # get list of installed apps
-installed_apps = []
 print('Generating list of installed apps... ', end='')
 
-for app in winapps.list_installed():
-    installed_apps.append(app)
 
+def installed_apps_list_generator():
+    app_list = []
+    for app in winapps.list_installed():
+        app_list.append(app)
+    return app_list
+
+
+installed_apps = installed_apps_list_generator()
 print('done.')
 
 # collect system process/pid/connections and save as process_list
-process_list = []
-local_connections_list = []
-remote_connections_list = []
 print('Generating list of system connections... ', end='')
 
-for i in psutil.net_connections():
-    try:
-        process_name = psutil.Process(i.pid).name()
-        process_pid = i.pid
 
-        if len(i.laddr) == 0:
-            local_ip = i.laddr
-            local_port = i.laddr
-        else:
-            local_ip = i.laddr.ip
-            local_port = i.laddr.port
+def process_list_generator():
+    proc_list = []
+    for i in psutil.net_connections():
+        try:
+            process_name = psutil.Process(i.pid).name()
+            process_pid = i.pid
 
-        if len(i.raddr) == 0:
-            remote_ip = i.raddr
-            remote_port = i.raddr
-        else:
-            remote_ip = i.raddr.ip
-            remote_port = i.raddr.port
+            if len(i.laddr) == 0:
+                local_ip = i.laddr
+                local_port = i.laddr
+            else:
+                local_ip = i.laddr.ip
+                local_port = i.laddr.port
 
-        status = i.status
+            if len(i.raddr) == 0:
+                remote_ip = i.raddr
+                remote_port = i.raddr
+            else:
+                remote_ip = i.raddr.ip
+                remote_port = i.raddr.port
 
-        process_list.append(
-            {
-                "name": process_name,
-                "pid": process_pid,
-                "local_ip": local_ip,
-                "local_port": local_port,
-                "remote_ip": remote_ip,
-                "remote_port": remote_port,
-                "status": status
-            }
-        )
+            status = i.status
 
-    except psutil.NoSuchProcess:
-        continue
+            proc_list.append(
+                {
+                    "name": process_name,
+                    "pid": process_pid,
+                    "local_ip": local_ip,
+                    "local_port": local_port,
+                    "remote_ip": remote_ip,
+                    "remote_port": remote_port,
+                    "status": status
+                }
+            )
+
+        except psutil.NoSuchProcess:
+            continue
+    return proc_list
+
 
 print('done.')
+process_list = process_list_generator()
 
 
 # publish results
@@ -75,22 +83,32 @@ config = configparser.ConfigParser()
 config.read('setup_usage.ini')
 setup_name = config['setup_information']['setup_name']
 
+
 # Vendor Support
-for process in process_list:
-    if process["name"] == "TeamViewer_Desktop.exe":  # if TeamViewer active session
-        print("TeamViewer session is active - reporting 'Vendor Support' status...")
-        time.sleep(2)
-        publish_setup_status(setup_name, 'vendor_support')
+def vendor_support_checker():
+    for process in process_list:
+        if process["name"] == "TeamViewer_Desktop.exe":  # if TeamViewer active session
+            print("TeamViewer session is active - reporting 'Vendor Support' status...")
+            time.sleep(2)
+            publish_setup_status(setup_name, 'vendor_support')
+
+
+vendor_support_checker()
+
 
 # Remote Testing
-chrome_remote_desktop_pids = 0
-for process in process_list:
-    if process["name"] == "remoting_host.exe":  # check for Chrome Remote Desktop/Remote Assistance active session
-        chrome_remote_desktop_pids += 1
-        if chrome_remote_desktop_pids > 7:
-            print("Chrome Remote Desktop session is active - reporting 'Remote Test' status...")
-            time.sleep(2)
-            publish_setup_status(setup_name, 'remote_testing')
+def remote_desktop_checker():
+    chrome_remote_desktop_pids = 0
+    for process in process_list:
+        if process["name"] == "remoting_host.exe":  # check for Chrome Remote Desktop/Remote Assistance active session
+            chrome_remote_desktop_pids += 1
+            if chrome_remote_desktop_pids > 7:
+                print("Chrome Remote Desktop session is active - reporting 'Remote Test' status...")
+                time.sleep(2)
+                publish_setup_status(setup_name, 'remote_testing')
+
+
+remote_desktop_checker()
 
 
 # automation / manual
@@ -152,10 +170,8 @@ def check_connection(source_process_name, destination_process_name,
 
 # check for IDLE setup
 def idle_time_check():
-    """Determines if a Windows system idle time exceeds 20 minutes."""
 
     def get_idle_duration():
-        """Retrieves system idle time from user32.dll."""
 
         class LastInputInfo(Structure):
             _fields_ = [
@@ -179,19 +195,21 @@ def idle_time_check():
 vendor = ''
 print('Identifying current setup... ', end='')
 
-# Anritsu setup check
+# Anritsu setup check ########
 if setup_vendor(installed_apps, 'Rapid Test Designer', 'Common Interface Driver'):
     vendor = 'anritsu'
     print('Setup identified as: Anritsu.')
     pass
 
-# Keysight setup check
+# Keysight setup check ########
 elif setup_vendor(installed_apps, 'Anite Automation', 'Anite Licensing', 'Keysight Core', 'Keysight SAS'):
     vendor = 'keysight'
     print('Setup identified as: Keysight.')
+
     # check for Keysight test
     print('Checking for test in Keysight... ')
     keysight_test_running = check_connection('SASLTESequencer.exe', 'AniteAutomationController.exe', process_list)
+
     # check for automated test
     keysight_automation = check_connection('SASTestManager.exe', 'RCMISvr.exe', process_list)
 
@@ -202,14 +220,35 @@ elif setup_vendor(installed_apps, 'Anite Automation', 'Anite Licensing', 'Keysig
         else:
             print('Keysight setup performing manual testing. Reporting... ')
             publish_setup_status(vendor, 'manual')
-    else:
-        pass
 
-# Rohde-Schwarz check
+# Rohde-Schwarz check ########
 elif setup_vendor(installed_apps, 'R&S CMW1'):
     vendor = 'rohde-schwarz'
+
+    # check for Rohde-Schwarz test
+    def rohde_schwarz_contest_instance_counter():
+        count = 0
+        for process in process_list:
+            if process['name'] == 'RohdeSchwarz.Contest.exe':
+                if process['remote_port'] not in [5442, 9443]:
+                    if process['status'] == 'ESTABLISHED':
+                        count += 1
+            return count
+
+    rohde_schwarz_contest_instances = rohde_schwarz_contest_instance_counter()
+    rohde_schwarz_test_running = rohde_schwarz_contest_instances >= 3
+
+    # check for automated test
+    rohde_schwarz_automation = check_connection('RohdeSchwarz.Contest.exe', 'AutoMgr.exe', process_port=4754)
     print('Setup identified as: Rohde-Schwarz.')
-    pass
+
+    if rohde_schwarz_test_running:
+        if rohde_schwarz_automation:
+            print('Rohde-Schwarz setup performing automated testing. Reporting... ')
+            publish_setup_status(vendor, 'automation')
+        else:
+            print('Rohde-Schwarz setup performing manual testing. Reporting... ')
+            publish_setup_status(vendor, 'manual')
 
 elif setup_vendor(installed_apps, 'Intel'):  # dummy
     vendor = 'intel'
